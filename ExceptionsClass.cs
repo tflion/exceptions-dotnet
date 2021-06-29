3d43e873a659abef78a459bfc052a233f3e0a16a
tflion <tfleao07@gmail.com>
2021-06-29 19:58:11 -0300

    iniciando-com-exceptions

---
+++ ExceptionsClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AulaExceptions
+{
+    public class ExceptionsClass
+    {
+        public static int Dividir(int numero, int divisor)
+        {
+            return numero / divisor;
+        }
+
+        public void Utilizar()
+        {
+            TestaDivisao(0);
+        }
+
+        public void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿ using System;
+
+namespace AulaExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           ExceptionsClass exceptions = new ExceptionsClass();
+
+            try
+            {
+                exceptions.Utilizar();
+            }catch(DivideByZeroException error)
+            {
+                Console.WriteLine("Ocorreu a seguinte exceção: (" + error.Message + ")");
+                Console.WriteLine("Caminho da exceção: " + error.StackTrace);
+            }
+
+            Console.WriteLine("Exceção tratada!");
+
+            Console.ReadLine();
+        }
+    }
+}
